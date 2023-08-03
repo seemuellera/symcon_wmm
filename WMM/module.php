d9c7d1e73260b3c395700ebe25b380241cd864e0
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 22:12:17 +0200

    sdf

--- WMM/module.php
+++ WMM/module.php
@@ -119,9 +119,12 @@ class WMM extends IPSModule {
 
 		// Clean old message registration
 		$messagesList = $this->GetMessageList();
-		foreach ($messagesList as $currentMessage) {
+		foreach ($messagesList as $currentMessageVarId => $currentMessageIDs) {
 
-			$this->UnregisterMessage($currentMessage, VM_UPDATE);
+			foreach ($currentMessageIDs as $currentMessageID) {
+
+				$this->UnregisterMessage($currentMessageVarId, $currentMessageID);
+			}
 		}
 		
 		// Register Variables
